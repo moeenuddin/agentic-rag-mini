d8c418a5144ef14c1fc6c42e45a2c1cd84c827aa
Muhammad Moeenuddin <getmoeen@gmail.com>
2026-01-16 21:17:59 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+api_key = st.secrets["openai"]["api_key"]
 MODEL_NAME = "gpt-4o-mini"
