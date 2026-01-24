524096c31846c76f1aafb05a32ca9e56c3e40a31
Muhammad Moeenuddin <getmoeen@gmail.com>
2026-01-24 15:34:03 +0500

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ load_dotenv()
 #OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 #api_key = st.secrets["openai"]["api_key"]
-os.environ["OPENAI_API_KEY"] = st.secrets["openai"]["api_key"]
+os.environ["OPENAI_API_KEY"] = st.secrets["openai"]
 
 MODEL_NAME = "gpt-4o-mini"
